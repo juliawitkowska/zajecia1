4820099f2336e36625c72f4f2982717b3125cb75
juliawitkowska <jwitkowska15.st.swpd.edu.pl>
2025-12-05 12:50:28 +0100

    lab08

--- gr_2_zajecia/biblioteka/urls.py
+++ gr_2_zajecia/biblioteka/urls.py
@@ -5,3 +5,14 @@ urlpatterns = [
     path('books/', views.book_list),
     path('books/<int:pk>/', views.book_detail),
 ]
+
+from django.urls import path
+
+# importujemy moduł views (plik views.py z tego samego katalogu co plik bieżący)
+from . import views
+
+# definiujemy zmienną urlpatterns, która jest listą mapowań adresów URL na nasze widoki
+urlpatterns = [
+    path("welcome/", views.welcome_view),
+    path("html/osoby/", views.osoba_list_html, name="osoba-list"),
+]
